bd4e27be02ff673ccc7ce2d942d6e8e28686d5c0
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2026-01-10 00:11:14 +0100

    Update highlightRewr.js

--- highlightRewr.js
+++ highlightRewr.js
@@ -1,3 +1,5 @@
+// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed
+
 document.addEventListener('DOMContentLoaded', () => {
   const card = document.querySelectorAll(.card); 
 
