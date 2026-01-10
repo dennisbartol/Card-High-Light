b703d75f7537c1d11a12dc48643a33adbc004aa1
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2026-01-10 15:31:31 +0100

    Update highlightRewr.js

--- highlightRewr.js
+++ highlightRewr.js
@@ -13,6 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // The getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
     // The getBoundingClientRect() method also returns a DOMRect object with eight properties: left, top, right, bottom, x, y, width, height.
 
+      const x = e.clientX - rect.left; 
+      const y = e.clientY - rect.top; 
 
     }
 
